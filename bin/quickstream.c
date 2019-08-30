ff4ad7a72e00a05ed9ca981c931f2ea69008da32
Lance Arsenault <lanceman@vt.edu>
2019-08-30 13:37:22 -0400

    Hi KG

--- bin/quickstream.c
+++ bin/quickstream.c
@@ -1,29 +1,91 @@
-/** \page quickstream_program quickstream 
- *
- * \brief stream runner program
- 
-\tableofcontents
-quickstream is a command-line program that can load, connect filters, and
-run the loaded filters in a flow stream
-
-\section quickstream_args command-line arguements
-
-\section quickstream_examples quickstream examples
-
-\subsection quickstream_helloworld quickstream helloworld
-
-
-*/
-
-
-
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 
 #include "../include/qsapp.h"
 
+// Turn on spew macros for this file.
+#ifndef SPEW_LEVEL_DEBUG
+#  define SPEW_LEVEL_DEBUG
+#endif
+#include "../lib/debug.h"
+
+
+
+static void catcher(int signum) {
+
+    WARN("Caught signal %d\n", signum);
+    while(1) usleep(10000);
+}
+
+
+
+
 int main(void) {
 
-    printf("hello quickstream version %s\n", QS_VERSION);
+// TODO: rewrite this code.   Currently it's just a "run test".
+
+    signal(SIGSEGV, catcher);
+
+    INFO("hello quickstream version %s", QS_VERSION);
+
+    struct QsApp *app = qsAppCreate();
+
+    struct QsFilter *f0, *f1, *f2;
+
+    if(!(f0 = qsAppFilterLoad(app, "stdin.so", 0))) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    if(!(f2 = qsAppFilterLoad(app, "stdin.so", 0))) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    f1 = qsAppFilterLoad(app, "stdout.so", "stdout");
+
+    if(!f1) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+
+    struct QsStream *stream = qsAppStreamCreate(app);
+    if(!stream) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    if(qsStreamConnectFilters(stream, f0, f1)) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    if(qsStreamConnectFilters(stream, f2, f1)) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    if(qsStreamConnectFilters(stream, f2, f1)) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+    if(qsStreamConnectFilters(stream, f1, f2)) {
+        qsAppDestroy(app);
+        return 1;
+    }
+
+
+    qsAppDisplayFlowImage(app, false);
+
+    qsStreamDestroy(stream);
+
+    qsAppDestroy(app);
+
+
+    WARN("SUCCESS");
 
     return 0;
 }
