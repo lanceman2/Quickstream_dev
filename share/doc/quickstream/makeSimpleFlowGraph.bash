901756d4e13f3d818b127227da77cfb705b05061
Lance Arsenault <lanceman@vt.edu>
2020-02-17 09:00:31 -0500

    Saving Work

--- share/doc/quickstream/makeSimpleFlowGraph.bash
+++ share/doc/quickstream/makeSimpleFlowGraph.bash
@@ -5,9 +5,9 @@ set -eo pipefail
 cd "$(dirname ${BASH_SOURCE[0]})"
 
 ../../../bin/quickstream\
- -f stdin [ --name input ]\
- -f tests/copy [ --name transWarpConduit ]\
- -f tests/copy [ --name malulator ]\
- -f stdout [ --name output ]\
+ -f stdin { --name input }\
+ -f tests/copy { --name transWarpConduit }\
+ -f tests/copy { --name malulator }\
+ -f stdout { --name output }\
  -c\
  -g
